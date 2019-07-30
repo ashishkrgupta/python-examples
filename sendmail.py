7325ff079b02ee4fcb65941e4b541230be0c57cf
Ashish Kumar Gupta <ashishkrgupta@hotmail.com>
2019-07-30 15:47:07 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -9,7 +9,7 @@ password = input("Enter your password: ")
 receiver = input("Email to whome you want to send message: ")
 message = input("Message to be sent: ")
 ctx = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, smtp_server, ctx) as server:
+with smtplib.SMTP_SSL(smtp_server, gmail_port, context=ctx) as server:
   server.login(sender, password)
   server.sendmail(sender, receiver, message)
   print("mail sent")
